9f0c228eaa6ab74bd8e96a6856937aefb5b467e4
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-05-04 10:51:09 -0400

    moved logic from login.jsp to login servlet

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServler", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("./login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (username.equals("admin") && password.equals("password")) {
+            resp.sendRedirect("/profile");
+        } else {
+            resp.sendRedirect("/login");
+        }
+    }
+}
